65ce745972383edbabc40160e9738a703850b2a4
David M Gudeman <davidmgudeman@gmail.com>
2023-03-02 17:00:58 -0800

    finish association exercise

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,10 +10,28 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+  
 
    has_many :enrollments,
        primary_key: :id,
        foreign_key: :course_id,
        class_name: :Enrollment
+
+    belongs_to :prerequisite,
+       primary_key: :id,
+       foreign_key: :prereq_id,
+       class_name: :Course,
+       optional: true
+
+    belongs_to :instructor,
+       primary_key: :id,
+       foreign_key: :instructor_id,
+       class_name: :User
+
+    
+    has_many :prerequisites,
+       primary_key: :id,
+       foreign_key: :prereq_id,
+       class_name: :Course
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,14 @@ class User < ApplicationRecord
        foreign_key: :student_id,
        class_name: :Enrollment
 
-    has_many :enrolled_courses, 
-        through: :enrollments
-        source: :course 
+    has_many :courses,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :Course
+       
+
+    has_many :enrolled_courses,
+       through: :enrollments,
+       source: :course
+       
 end
